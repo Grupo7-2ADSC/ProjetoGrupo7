9553aea76d2b64be53bae953384b7c3466520b93
GustavoDSFerreira <gutoshina@gmail.com>
2024-03-21 13:34:01 -0300

    Ajuste na validação de login

--- TesteCadastro/logUser/src/main/java/ModelLog.java
+++ TesteCadastro/logUser/src/main/java/ModelLog.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ModelLog {
     static Scanner scanner = new Scanner(System.in);
+    static List<Usuario> usuariosRegistrados = new ArrayList<>();
+
 
 
     public void lerUserSenha() {
@@ -12,7 +16,7 @@ public class ModelLog {
                 2 - Login
                 3 - Nada""");
         int escolha = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer do scanner pra pegar numero da ação
+        scanner.nextLine(); // Limpar o buffer do scanner para pegar o número da ação
 
         switch (escolha) {
             case 1:
@@ -25,6 +29,7 @@ public class ModelLog {
                 System.out.println("Operação cancelada.");
                 break;
             default:
+
                 System.out.println("Opção inválida.");
                 break;
         }
@@ -38,9 +43,15 @@ public class ModelLog {
         System.out.print("Digite a senha: ");
         String senha = scanner.nextLine();
 
+        Usuario novoUsuario = new Usuario(nomeUsuario, senha);
+        usuariosRegistrados.add(novoUsuario);
+
         System.out.println("Usuário cadastrado com sucesso!");
         System.out.println("Nome de usuário: " + nomeUsuario);
         System.out.println("Senha: " + senha);
+
+        // Voltar para o menu após o cadastro
+        lerUserSenha();
     }
 
     public void login() {
@@ -51,10 +62,40 @@ public class ModelLog {
         System.out.print("Digite a senha: ");
         String senha = scanner.nextLine();
 
-        // falta aplicar a logica de validação e ver se quer adicionar numa lista pra validar cadastro
-        System.out.println("Login realizado com sucesso!");
-        System.out.println("Nome de usuário: " + nomeUsuario);
-        System.out.println("Senha: " + senha);
+        boolean usuarioValido = false;
+        for (Usuario usuario : usuariosRegistrados) {
+            if (usuario.getNomeUsuario().equals(nomeUsuario) && usuario.getSenha().equals(senha)) {
+                usuarioValido = true;
+                break;
+            }
+        }
+
+        if (usuarioValido) {
+            System.out.println("Login realizado com sucesso!");
+
+        } else {
+            System.out.println("Usuário ou senha incorretos. \n Tente novamente ");
+            lerUserSenha();
+        }
+
+
+    }
+
+    static class Usuario {
+        private String nomeUsuario;
+        private String senha;
+
+        public Usuario(String nomeUsuario, String senha) {
+            this.nomeUsuario = nomeUsuario;
+            this.senha = senha;
+        }
+
+        public String getNomeUsuario() {
+            return nomeUsuario;
+        }
+
+        public String getSenha() {
+            return senha;
+        }
     }
 }
-
